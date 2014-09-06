db3f50de33834984f53d874130235996afc35d0b
tatsuhiko yamamura <tatsuhiko@unity3d.com>
2014-09-06 23:35:12 +0900

    first commit (without vuforia)

---
+++ Assets/ARCameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ARCameraController : MonoBehaviour
+{
+	private Texture2D backgroundTexture;
+	public Material backgroundMat;
+
+	void Start ()
+	{
+		QCARRenderer.Instance.DrawVideoBackground = false;
+
+		backgroundTexture = new Texture2D(0, 0, TextureFormat.RGB565, false);
+		backgroundTexture.filterMode = FilterMode.Point;
+		backgroundTexture.wrapMode = TextureWrapMode.Repeat;
+
+		QCARRenderer.Instance.SetVideoBackgroundTexture(backgroundTexture);
+
+		backgroundMat.mainTexture = backgroundTexture;
+	}
+}
